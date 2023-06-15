3356bbfc2a5a92860de2f2ef26003a122cdcf94c
Sravi-B <136445091+Sravi-B@users.noreply.github.com>
2023-06-15 17:15:21 +0530

    Update app_trial.py
    Adding multiselect

--- app_trial.py
+++ app_trial.py
@@ -9,5 +9,10 @@ streamlit.text('🥚🍳Hard-Boiled free range egg')
 streamlit.header('🍌🍊Build your own Smoothie...🍍🍏🥑')
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
