b45549805593e766d03635e022257e116df834a2
Sravi-B <136445091+Sravi-B@users.noreply.github.com>
2023-06-15 16:55:13 +0530

    Update app_trial.py
    added emojies and another header

--- app_trial.py
+++ app_trial.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title("My parents' new Diner")
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled free range egg')
+streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚🍳Hard-Boiled free range egg')
+
+
+streamlit.header('🍌🍊Build your own Smoothie...🍍🍏🥑')
