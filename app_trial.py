efde573a02268290c4ea69d94387ea4ec52229f7
Sravi-B <136445091+Sravi-B@users.noreply.github.com>
2023-06-15 16:49:12 +0530

    Update app_trial.py
    Added breakfast options

--- app_trial.py
+++ app_trial.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parents' new Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamliyt.text('Hard-Boiled free range egg')
