a1a0594b5fd549ba6c2ebcb6aeb9ad2417cdd591
Sravi-B <136445091+Sravi-B@users.noreply.github.com>
2023-06-19 11:45:39 +0530

    Update app_trial.py
    changed pandas to pd

--- app_trial.py
+++ app_trial.py
@@ -24,7 +24,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 # normalising the json version 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # shwoing it as a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
